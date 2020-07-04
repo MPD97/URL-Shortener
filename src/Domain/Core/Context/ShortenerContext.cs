dae99ce6e64a8560e6f2b1efb7e2bc51fb867356
MPD97 <mateuszpracadom@gmail.com>
2020-07-04 19:38:43 +0200

    Created ShortenerContext.cs

---
+++ src/Domain/Core/Context/ShortenerContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Core.Context
+{
+    public class ShortenerContext :DbContext
+    {
+        public ShortenerContext()
+        {
+            
+        }
+
+        public ShortenerContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
