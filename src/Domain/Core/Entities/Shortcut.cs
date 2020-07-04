e86bc13ad1de8148567357df10734a1af99f5024
MPD97 <mateuszpracadom@gmail.com>
2020-07-04 19:29:04 +0200

    Created Shortcut.cs

---
+++ src/Domain/Core/Entities/Shortcut.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Entities
+{
+    public class Shortcut
+    {
+        public long ShortcutId { get; set; }
+        public string Alias { get; set; }
+        public string Redirect { get; set; }
+    }
+}
