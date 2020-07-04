8a5fdf98eb85846cc69ae1fe556b74a536592fe1
MPD97 <mateuszpracadom@gmail.com>
2020-07-04 22:56:40 +0200

    Added AddedFluentApi migration

--- src/Domain/Core/Migrations/20200704205537_AddedFluentApi.Designer.cs
+++ src/Domain/Core/Migrations/20200704205537_AddedFluentApi.Designer.cs
@@ -3,14 +3,16 @@ using Core.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Core.Migrations
 {
     [DbContext(typeof(ShortenerContext))]
-    partial class ShortenerContextModelSnapshot : ModelSnapshot
+    [Migration("20200704205537_AddedFluentApi")]
+    partial class AddedFluentApi
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
