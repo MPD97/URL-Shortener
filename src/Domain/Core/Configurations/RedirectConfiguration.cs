22df228db5d767f1d630a5fe48384c82ebe37374
MPD97 <mateuszpracadom@gmail.com>
2020-07-04 22:37:12 +0200

    Added RedirectConfiguration.cs

---
+++ src/Domain/Core/Configurations/RedirectConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Core.Configurations
+{
+    public class RedirectConfiguration : IEntityTypeConfiguration<Redirect>
+    {
+        public void Configure(EntityTypeBuilder<Redirect> builder)
+        {
+            builder.HasKey(a => a.RedirectId);
+
+            builder.Property(a => a.Url)
+                .HasMaxLength(80)
+                .IsRequired(true);
+
+            builder
+                .HasOne(a => a.Shortcut)
+                .WithOne(a => a.Redirect);
+        }
+    }
+}
