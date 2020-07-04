7b0037397a15f2fa9cb61fad110987e026e7c5fd
MPD97 <mateuszpracadom@gmail.com>
2020-07-04 19:43:16 +0200

    Added Redirect DbSet

--- src/Domain/Core/Context/ShortenerContext.cs
+++ src/Domain/Core/Context/ShortenerContext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Core.Context
 {
     public class ShortenerContext :DbContext
     {
+        public DbSet<Redirect> Redirects { get; set; }    
         public ShortenerContext()
         {
             
