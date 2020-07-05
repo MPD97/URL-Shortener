33d5d60e628a792ca00ee86d8ce3bb85c363c0f5
MPD97 <mateuszpracadom@gmail.com>
2020-07-05 11:56:16 +0200

    Created IShortcutRepository.cs

---
+++ src/Infrastructure/Presistance/Repositories/IShortcutRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Entities;
+
+namespace Presistance.Repositories
+{
+    public interface IShortcutRepository
+    {
+        public Task<Shortcut> FindByIdAsync(long id);
+        public Task<Shortcut> FindByAliasAsync(string alias);
+        public Task<long> GetCountAsync();
+        public Task<IEnumerable<Shortcut>> GetAllAsync();
+        public Task InsertAsync(Shortcut shortcut);
+        public Task<bool> DeleteAsync(long id);
+        public Task UpdateAsync(Shortcut shortcut);
+        public Task<int> SaveChangesAsync();
+    }
+}
