76a76d294538d3be40c1d7afd8e6b3b1f963498a
MPD97 <mateuszpracadom@gmail.com>
2020-07-05 14:00:57 +0200

    Created RedirectEfRepsitory.cs

---
+++ src/Infrastructure/Presistance/Repositories/RedirectEfRepsitory.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Entities;
+
+namespace Presistance.Repositories
+{
+    public class RedirectEfRepsitory :IRedirectRepsitory
+    {
+        public Task<Redirect> FindByIdAsync(long id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Redirect> FindByUrlAsync(string url)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<long> GetCountAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<List<Redirect>> GetAllAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<List<Redirect>> GetAllAsync(int take, int skip)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task InsertAsync(Redirect redirect)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task DeleteAsync(Redirect redirect)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task UpdateAsync(Redirect redirect)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<int> SaveChangesAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
