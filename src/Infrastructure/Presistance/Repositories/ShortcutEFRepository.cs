b659fe34ad189d6fbeac0d4d2d8329f9aef26b40
MPD97 <mateuszpracadom@gmail.com>
2020-07-05 12:01:45 +0200

    Created ShortcutEFRepository.cs

---
+++ src/Infrastructure/Presistance/Repositories/ShortcutEFRepository.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Entities;
+
+namespace Presistance.Repositories
+{
+    public class ShortcutEFRepository :IShortcutRepository
+    {
+        public Task<Shortcut> FindByIdAsync(long id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Shortcut> FindByAliasAsync(string alias)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<long> GetCountAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<Shortcut>> GetAllAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task InsertAsync(Shortcut shortcut)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> DeleteAsync(long id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task UpdateAsync(Shortcut shortcut)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<int> SaveChangesAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
