02b517fd94799081274ac874048866d04e37bda5
Mateusz <mateusz@o2.pl>
2020-07-06 11:30:30 +0200

    Created RedirectQuery.cs

---
+++ src/Infrastructure/Presistance/Services/RedirectQuery.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Core.Entities;
+using Presistance.Repositories;
+
+namespace Presistance.Services
+{
+    public class RedirectQuery : IRedirectQuery
+    {
+        private readonly IRedirectRepository _redirectRepository;
+
+        public RedirectQuery(IRedirectRepository redirectRepository)
+        {
+            _redirectRepository = redirectRepository;
+        }
+
+        public Task<Redirect> Find(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Redirect> Find(string url)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
