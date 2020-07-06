1256ba01572534511fd90911d5a6343f5ed6ca39
Mateusz <mateusz@o2.pl>
2020-07-06 12:02:13 +0200

    Created RedirectExtendedQuery.cs

---
+++ src/Infrastructure/Presistance/Services/RedirectExtendedQuery.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using Core.Entities;
+using Presistance.Repositories;
+
+namespace Presistance.Services
+{
+    public class RedirectExtendedQuery : IRedirectExtendedQuery
+    {
+        private readonly IRedirectExtendedRepository _redirectExtendedRepository;
+
+        public RedirectExtendedQuery(IRedirectExtendedRepository redirectExtendedRepository)
+        {
+            _redirectExtendedRepository = redirectExtendedRepository;
+        }
+
+        public Task<RedirectExtended> Find(long id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<RedirectExtended> Find(string url)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<RedirectExtended> FindAndInclude(long id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<RedirectExtended> FindAndInclude(string url)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<long> Count()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
