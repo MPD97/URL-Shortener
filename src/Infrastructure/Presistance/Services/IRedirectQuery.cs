5392617ee09b6147415ab1c931655fc40f2334b0
Mateusz <mateusz@o2.pl>
2020-07-06 11:29:23 +0200

    Created IRedirectQuery.cs

---
+++ src/Infrastructure/Presistance/Services/IRedirectQuery.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using Core.Entities;
+
+namespace Presistance.Services
+{
+    public interface IRedirectQuery
+    {
+        public Task<Redirect> Find(int id);
+        public Task<Redirect> Find(string url);
+    }
+}
