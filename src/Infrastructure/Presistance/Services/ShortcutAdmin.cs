0c3379e96ae7003d0be5ba72e102741c184ad83b
Mateusz <mateusz@o2.pl>
2020-07-06 12:15:30 +0200

    Created ShortcutAdmin.cs

---
+++ src/Infrastructure/Presistance/Services/ShortcutAdmin.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using Core.Entities;
+using Presistance.Repositories;
+
+namespace Presistance.Services
+{
+    public class ShortcutAdmin : IShortcutAdmin
+    {
+        private readonly IShortcutRepository _shortcutRepository;
+
+        public ShortcutAdmin(IShortcutRepository shortcutRepository)
+        {
+            _shortcutRepository = shortcutRepository;
+        }
+
+        public Task InsertAsync(Shortcut shortcut)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task DeleteAsync(Shortcut shortcut)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task UpdateAsync(Shortcut shortcut)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<int> SaveChangesAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
