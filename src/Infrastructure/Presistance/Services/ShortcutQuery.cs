eddcf7e365e038f2d715fba741418b70031107dd
Mateusz <mateusz@o2.pl>
2020-07-06 11:49:08 +0200

    Created ShortcutQuery.cs

---
+++ src/Infrastructure/Presistance/Services/ShortcutQuery.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using Core.Entities;
+using Presistance.Repositories;
+
+namespace Presistance.Services
+{
+    public class ShortcutQuery : IShortcutQuery
+    {
+        private readonly IShortcutRepository _shortcutRepository;
+
+        public ShortcutQuery(IShortcutRepository shortcutRepository)
+        {
+            _shortcutRepository = shortcutRepository;
+        }
+
+        public Task<Shortcut> Find(long id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Shortcut> Find(string alias)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Shortcut> FindAndInclude(long id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Shortcut> FindAndInclude(string alias)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Shortcut> Count()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
