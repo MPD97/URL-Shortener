d87977d11d2e807ea8cfe3d1b010009a5aaae486
Mateusz <mateusz@o2.pl>
2020-07-06 11:38:37 +0200

    Implemented IShortcutQuery.cs

---
+++ src/Infrastructure/Presistance/Services/IShortcutQuery.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Core.Entities;
+
+namespace Presistance.Services
+{
+    public interface IShortcutQuery
+    {
+        public Task<Shortcut> Find(long id);
+        public Task<Shortcut> Find(string alias);
+        public Task<Shortcut> FindAndInclude(long id);
+        public Task<Shortcut> FindAndInclude(string alias);
+        public Task<Shortcut> Count();
+    }
+}
