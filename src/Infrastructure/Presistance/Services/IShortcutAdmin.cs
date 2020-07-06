2bdaf9f268b9dcab5406b5e6b8505d5d798af350
Mateusz <mateusz@o2.pl>
2020-07-06 12:14:35 +0200

    Created IShortcutAdmin.cs

---
+++ src/Infrastructure/Presistance/Services/IShortcutAdmin.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Core.Entities;
+
+namespace Presistance.Services
+{
+    public interface IShortcutAdmin
+    {
+        public Task InsertAsync(Shortcut shortcut);
+        public Task DeleteAsync(Shortcut shortcut);
+        public Task UpdateAsync(Shortcut shortcut);
+        public Task<int> SaveChangesAsync();
+    }
+}
