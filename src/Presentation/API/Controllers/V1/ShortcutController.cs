17b45cc9cd139571e955168bef95858b9a2101d0
Mateusz <mateusz@o2.pl>
2020-07-07 09:42:41 +0200

    Implemented Get action

--- src/Presentation/API/Controllers/V1/ShortcutController.cs
+++ src/Presentation/API/Controllers/V1/ShortcutController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Threading.Tasks;
 using Core.DTOs.Requests;
 using Microsoft.AspNetCore.Mvc;
+using Presistance.Services;
 
 namespace API.Controllers.V1
 {
@@ -8,10 +9,27 @@ namespace API.Controllers.V1
     [Route("api/v1/[controller]")]
     public class ShortcutController : Controller
     {
+        private readonly IShortcutQuery _shortcutQuery;
+
+        public ShortcutController(IShortcutQuery shortcutQuery)
+        {
+            _shortcutQuery = shortcutQuery;
+        }
+
+        [HttpGet("/{id}")]
+        public async Task<IActionResult> Get(long id)
+        {
+            var result = await _shortcutQuery.Find(id);
+            if (result == null)
+            {
+                return BadRequest("Shortcut with this id not exists.");
+            }
+            return Ok(result);
+        }
         [HttpPost()]
         public async Task<IActionResult> Create(ShortcutCreateRequest request)
         {
-            return View();
+            return Ok();
         }
     }
 }
