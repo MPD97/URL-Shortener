db9d55f63d09da434503127814d822fe4c841e85
Mateusz <mateusz@o2.pl>
2020-07-07 08:09:18 +0200

    Created RedirectController.cs

---
+++ src/Presentation/API/Controllers/RedirectController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    
+    [ApiController]
+    [Route("api/v1")]
+    public class RedirectController : Controller
+    {
+
+    }
+}
