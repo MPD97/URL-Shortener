825002ca7fb5d3b069458658df701f08849de7be
Mateusz <mateusz@o2.pl>
2020-07-07 09:37:23 +0200

    Created ShortcutController.cs

---
+++ src/Presentation/API/Controllers/V1/ShortcutController.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Core.DTOs.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers.V1
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class ShortcutController : Controller
+    {
+        [HttpPost()]
+        public async Task<IActionResult> Create(ShortcutCreateRequest request)
+        {
+            return View();
+        }
+    }
+}
