4b9cde8c5fbec34782d38fa0a07b37855e4b76c6
Mateusz <mateusz@o2.pl>
2020-07-07 11:16:11 +0200

    Implemented Create Shortcut

--- src/Presentation/API/Controllers/V1/ShortcutController.cs
+++ src/Presentation/API/Controllers/V1/ShortcutController.cs
@@ -1,6 +1,10 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Core.DTOs.Requests;
+using Core.DTOs.Responses;
+using Core.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Presistance.Repositories;
 using Presistance.Services;
 
 namespace API.Controllers.V1
@@ -10,10 +14,19 @@ namespace API.Controllers.V1
     public class ShortcutController : Controller
     {
         private readonly IShortcutQuery _shortcutQuery;
+        private readonly IAliasGenerator _generator;
+        private readonly IShortcutAdmin _shortcutAdmin;
+        private readonly IRedirectQuery _redirectQuery;
+        private readonly IRedirectExtendedQuery _redirectExtendedQuery;
 
-        public ShortcutController(IShortcutQuery shortcutQuery)
+        public ShortcutController(IShortcutQuery shortcutQuery, IAliasGenerator generator, IShortcutAdmin shortcutAdmin,
+            IRedirectQuery redirectQuery, IRedirectExtendedQuery redirectExtendedQuery)
         {
             _shortcutQuery = shortcutQuery;
+            _generator = generator;
+            _shortcutAdmin = shortcutAdmin;
+            _redirectQuery = redirectQuery;
+            _redirectExtendedQuery = redirectExtendedQuery;
         }
 
         [HttpGet("{id}")]
@@ -24,12 +37,76 @@ namespace API.Controllers.V1
             {
                 return BadRequest("Shortcut with this id not exists.");
             }
+
             return Ok(result);
         }
+
         [HttpPost()]
         public async Task<IActionResult> Create(ShortcutCreateRequest request)
         {
-            return Ok();
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("ValidationError");
+            }
+            Uri uriResult;
+            bool validUrl = Uri.TryCreate(request.Url, UriKind.Absolute, out uriResult) 
+                          && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+            if (!validUrl)
+            {
+                return BadRequest("Url is not valid url");
+            }
+
+            var redirect = await _redirectQuery.Find(request.Url, true);
+            if (redirect != null)
+            {
+                return BadRequest($"This url exist. Use {redirect.Shortcut.Alias}");
+            }
+            
+            var redirectExtended = await _redirectExtendedQuery.Find(request.Url, true);
+            if (redirectExtended != null)
+            {
+                return BadRequest($"This url exist. Use {redirectExtended.Shortcut.Alias}");
+            }
+            
+            Shortcut shortcut = null;
+
+            if (!string.IsNullOrEmpty(request.Alias))
+            { 
+                shortcut = await _shortcutQuery.Find(request.Alias);
+                if (shortcut != null)
+                {
+                    return BadRequest("Shortcut with this alias exists.");
+                }
+
+                shortcut = await _shortcutAdmin.InsertAsync(request.Alias, request.Url);
+            }
+            else
+            {
+                int i = 1;
+                var alias = _generator.Generate(i);
+                while ((shortcut = await _shortcutQuery.Find(alias)) != null)
+                {
+                    if (i < 30)
+                    {
+                        i++;
+                    }
+
+                    alias = _generator.Generate(i);
+                }
+
+                shortcut = await _shortcutAdmin.InsertAsync(alias, request.Url);
+            }
+                            
+            var result = await _shortcutAdmin.SaveChangesAsync();
+            if (result <= 0)
+            {
+                return StatusCode(500, "Error");
+            }
+            return StatusCode(201, new ShortcutCreateResponse
+            {
+                Alias = shortcut.Alias,
+                Url = request.Url
+            });
         }
     }
 }
