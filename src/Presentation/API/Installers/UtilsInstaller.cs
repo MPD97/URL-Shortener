c274af10ff471f4438e2fe88b7c803bd7186e80f
Mateusz <mateusz@o2.pl>
2020-07-07 10:50:18 +0200

    Added UtilsInstaller.cs

---
+++ src/Presentation/API/Installers/UtilsInstaller.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Presistance.Services;
+
+namespace API.Installers
+{
+    public class UtilsInstaller : IInstaller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSingleton<IAliasGenerator, AliasGenerator>();
+        }
+    }
+}
