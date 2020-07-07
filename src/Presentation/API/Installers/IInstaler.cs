7de9a9892e2c2085e9a2f38abce966404899e61c
Mateusz <mateusz@o2.pl>
2020-07-07 08:12:23 +0200

    Created IInstaler

---
+++ src/Presentation/API/Installers/IInstaler.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace API.Installers
+{
+    public interface IInstaler
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration configuration);
+
+    }
+}
