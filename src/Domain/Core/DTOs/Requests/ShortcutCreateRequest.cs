d4496f809a5d59e1a110fad93e1ffcfdebc2d333
Mateusz <mateusz@o2.pl>
2020-07-07 09:28:53 +0200

    Added ShortcutCreateRequest.cs

---
+++ src/Domain/Core/DTOs/Requests/ShortcutCreateRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.DTOs.Requests
+{
+    public class ShortcutCreateRequest
+    {
+        [Required]
+        [MaxLength(1000)]
+        [DataType(DataType.Url)]
+        public string Url { get; set; }
+    }
+}
