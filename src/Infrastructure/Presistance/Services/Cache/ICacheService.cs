cb8f81f892726a70216173075b9afc55bf8fa480
Mateusz <mateusz@o2.pl>
2020-07-08 10:52:05 +0200

    Implemented RedisCacheServiceService.cs and renamed

--- src/Infrastructure/Presistance/Services/Cache/ICacheService.cs
+++ src/Infrastructure/Presistance/Services/Cache/ICacheService.cs
@@ -2,7 +2,7 @@
 
 namespace Presistance.Services.Cache
 {
-    public interface ICache
+    public interface ICacheService
     {
         public Task<string> GetCacheValueAsync(string key);
         public Task SetChacheValueAsync(string key, string value);
---
+++ src/Infrastructure/Presistance/Services/Cache/RedisCacheServiceService.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using StackExchange.Redis;
+
+namespace Presistance.Services.Cache
+{
+    public class RedisCacheServiceService : ICacheService
+    {
+        private readonly IConnectionMultiplexer _multiplexer;
+
+        public RedisCacheServiceService(IConnectionMultiplexer multiplexer)
+        {
+            _multiplexer = multiplexer;
+        }
+
+        public async Task<string> GetCacheValueAsync(string key)
+        {
+            var database = _multiplexer.GetDatabase();
+            return await database.StringGetAsync(key);
+        }
+
+        public async Task SetChacheValueAsync(string key, string value)
+        {
+            var database = _multiplexer.GetDatabase();
+            await database.StringSetAsync(key, value);
+        }
+    }
+}
