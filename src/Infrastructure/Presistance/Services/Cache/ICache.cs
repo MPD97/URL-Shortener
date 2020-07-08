0503d20909758b807e908600d93e027214738fe7
Mateusz <mateusz@o2.pl>
2020-07-08 10:42:43 +0200

    Created ICache interface

---
+++ src/Infrastructure/Presistance/Services/Cache/ICache.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace Presistance.Services.Cache
+{
+    public interface ICache
+    {
+        public Task<string> GetCacheValueAsync(string key);
+        public Task SetChacheValueAsync(string key, string value);
+    }
+}
